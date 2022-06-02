5f46bffb1cd080a59405297d90d7ba56e05e4a4e
jamesjnbaoa <jemesnavarro@aoacolombia.com>
2022-06-01 21:03:37 -0500

    correo

---
+++ hojavida.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+
+<!--[if !IE]><!-->
+
+<html lang="es" id="eFX" class="">
+
+<!--<![endif]-->
+
+<head>
+
+	<meta charset="utf-8" />
+
+	<title>Trabaja con nosotros</title>
+
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+
+	<meta name="keywords" content="freddy xiques, efx, admin.efx, compra.efx, emprendedores, diseño y desarrollo web, diseño y desarrollo de aplicaciones móviles, diseño de páginas web informativas, publicidad y marketing digita, paginas web, diseño web, diseno web, hosting, website design, diseño web joomla, apps, atlántico, barranquilla, colombia, email marketing, posicionamiento, empresa diseño web, diseño web economico, diseño web barato, diseño web costoso" />
+
+	<meta content="" name="description" />
+
+	<meta content="" name="author" />
+
+
+
+	<!-- ================== BEGIN BASE CSS STYLE ================== -->
+
+	<link rel="icon" href="assets/img/logonew.png">
+
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+
+	<link href="assets/plugins/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
+
+	<link href="assets/plugins/font-awesome/5.3/css/all.min.css" rel="stylesheet" />
+
+	<link href="assets/plugins/animate/animate.min.css" rel="stylesheet" />
+
+	<link href="assets/css/one-page-parallax/style.min.css" rel="stylesheet" />
+
+	<link href="assets/css/one-page-parallax/style-responsive.min.css" rel="stylesheet" />
+
+	<link href="assets/css/one-page-parallax/theme/green.css" id="theme" rel="stylesheet" />
+
+	<!-- ================== END BASE CSS STYLE ================== -->
+
+
+
+	<!-- ================== BEGIN BASE JS ================== -->
+
+	<script src="assets/plugins/pace/pace.min.js"></script>
+
+	<!-- ================== END BASE JS ================== -->
+
+</head>
+
+<body data-spy="scroll" data-target="#header" data-offset="51">
+
+	<!-- begin #page-container -->
+
+	<div id="page-container" class="fade"><br><br>
+
+		<!-- begin #header -->
+
+		<div id="header" class="header navbar navbar-transparent navbar-fixed-top">
+
+			<!-- begin container -->
+
+			<div class="container">
+
+				<!-- begin navbar-header -->
+
+				<div class="navbar-header">
+
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#header-navbar">
+
+						<span class="icon-bar"></span>
+
+						<span class="icon-bar"></span>
+
+						<span class="icon-bar"></span>
+
+					</button>
+
+					<a href="/" class="navbar-brand">
+
+
+						<img src="assets/img/logofinal.png">
+						<span class="brand-text">
+
+						</span>
+
+					</a>
+
+				</div>
+
+				<!-- end navbar-header -->
+
+
+				<div class="collapse navbar-collapse" id="header-navbar">
+
+
+					<ul class="nav navbar-nav navbar-right">
+						<li style="text-align: left;" class="nav-item"><a class="nav-link"></a></li>
+
+					</ul>
+
+				</div>
+
+				<!-- end navbar-collapse -->
+				<div class="collapse navbar-collapse" id="header-navbar">
+
+					<ul class="nav navbar-nav navbar-right">
+
+						<li class="nav-item"><a class="nav-link" href="index.html">Inicio</a></li>
+
+
+					</ul>
+
+				</div>
+
+			</div>
+
+			<!-- end container -->
+
+		</div>
+
+
+
+		<div id="home" class="content" data-scrollview="true">
+
+
+			<!-- begin container -->
+
+			<div class="container" data-animation="true" data-animation-type="fadeInDown">
+
+				<div id="home" class="content has-bg home">
+
+					<!-- begin content-bg -->
+
+					<div class="content-bg" style="background-image: url(assets/img/bg/cover7.jpg);" data-paroller="true" data-paroller-factor="0.5" data-paroller-factor-xs="0.25">
+
+					</div>
+
+					<!-- end content-bg -->
+
+
+
+					<div class="container" data-animation="true" data-animation-type="fadeInLeft">
+
+						<!-- begin row -->
+
+						<div class="row">
+
+							<!-- begin col-12 -->
+
+							<div class="col-md-12 quote">
+
+								<h3 style="padding-top: 6px;">INGRESA TU INFORMACION PARA CONTACTARTE</h3>
+
+							</div>
+
+							
+
+							<!-- end col-12 -->
+
+						</div>
+
+						<!-- end row -->
+
+					</div>
+
+
+
+				</div>
+
+
+				<div>
+
+				<div class="col-md-12 quote">
+								<form name="formulario" id="formulario" action="enviarOne.php" method="post" enctype="multipart/form-data">
+									<div class="row justify-content-center">
+										<div class="col-6">
+											<div class="form-group">
+												<label for="nombre">Nombre Completo</label>
+												<input type="text" name="nombre" placeholder="Escriba su Nombre" class="form-control" required="true">
+											</div>
+										</div>
+										<div class="col-6">
+											<div class="form-group">
+												<label for="email">Email</label>
+												<input type="email" name="email" placeholder="Su email" class="form-control">
+											</div>
+										</div>
+									</div>
+
+									<div class="input-group mb-3">
+										<div class="input-group-prepend">
+											<span class="input-group-text">Archivo</span>
+										</div>
+										<div class="custom-file">
+											<input type="file" name="my_file" id="my_file" class="custom-file-input" multiple>
+											<label class="custom-file-label" for="inputGroupFile01">Hacer click aqui..!</label>
+										</div>
+									</div>
+
+									<div class="col-12 text-center mt-5 mb-5">
+										<button type="submit" class="btn btn-primary">Enviar Archivo Adjunto</button>
+									</div>
+								</form>
+							</div>
+				</div><br>
+
+				<!-- end row -->
+
+			</div>
+
+
+			<div id="quote" class="content bg-black-darker has-bg" data-scrollview="true">
+
+				<!-- begin content-bg -->
+
+				<div class="content-bg" style="background-image: url(assets/img/empresarial.jpg)" data-paroller-factor="0.5" data-paroller-factor-md="0.01" data-paroller-factor-xs="0.01">
+
+				</div>
+
+				<!-- end content-bg -->
+
+				<!-- begin container -->
+
+				<div class="container" data-animation="true" data-animation-type="fadeInLeft">
+
+					<!-- begin row -->
+
+					<div class="row">
+
+						<!-- begin col-12 -->
+
+						<div class="col-md-12 quote">
+
+
+							<a href="/" class="navbar-brand">
+								<img style="width: 280px;" src="assets/img/signika.png">
+
+							</a>
+
+
+							<!-- <small><a class="nav-link" href="#team" data-click="scroll-to-target">Equipo de trabajo TransOriente S.A.S</a></small> -->
+
+						</div>
+
+						<!-- end col-12 -->
+
+					</div>
+
+					<!-- end row -->
+
+				</div>
+
+				<!-- end container -->
+
+			</div>
+			<!-- end container -->
+
+
+		</div>
+
+
+
+		<!-- end #about -->
+
+
+
+
+
+		<!-- begin #footer -->
+
+
+
+		<!-- end #footer -->
+
+
+
+		<!-- begin theme-panel -->
+
+
+
+		<!-- end theme-panel -->
+
+
+
+
+		<!-- end #page-container -->
+
+
+
+		<!-- ================== BEGIN BASE JS ================== -->
+
+		<script src="assets/plugins/jquery/jquery-3.3.1.min.js"></script>
+
+		<script src="assets/plugins/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+
+		<!--[if lt IE 9]>
+
+		<script src="assets/crossbrowserjs/html5shiv.js"></script>
+
+		<script src="assets/crossbrowserjs/respond.min.js"></script>
+
+		<script src="assets/crossbrowserjs/excanvas.min.js"></script>
+
+	<![endif]-->
+
+		<script src="assets/plugins/js-cookie/js.cookie.js"></script>
+
+		<script src="assets/plugins/scrollMonitor/scrollMonitor.js"></script>
+
+		<script src="assets/plugins/paroller/jquery.paroller.min.js"></script>
+
+		<script src="assets/js/one-page-parallax/apps.min.js"></script>
+
+		<!-- ================== END BASE JS ================== -->
+
+
+
+		<script>
+			$(document).ready(function() {
+
+				App.init();
+
+			});
+		</script>
+
+
+</body>
+
+</html>
